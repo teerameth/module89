47f7d732adad64b508528ce1f6473ba6eb6fb34a
Wrenchy <36445140+teerameth@users.noreply.github.com>
2022-04-28 10:21:59 +0700

    Use 1080p -> 480p scaling on onnx_inference

--- scripts/onnx_inference_1080p.py
+++ scripts/onnx_inference_1080p.py
@@ -7,8 +7,10 @@ import simplejson
 import time
 
 config = simplejson.load(open(os.path.join('../config/camera_config.json')))
-cameraMatrix = np.array(config['camera_matrix'], np.float32)
-dist = np.array(config['dist'])
+# cameraMatrix = np.array(config['camera_matrix'], np.float32)
+cameraMatrix = np.array([[1395.3709390074625, 0.0, 984.6248356317226], [0.0, 1396.2122002126725, 534.9517311724618], [0.0, 0.0, 1.0]], np.float32)
+# dist = np.array(config['dist'])
+dist = np.array([[0.1097213194870457, -0.1989645299789654, -0.002106454674127449, 0.004428959364733587, 0.06865838341764481]], np.float32)
 based_obj_points = np.array([[-0.2, 0.23, 0], [-0.2, -0.23, 0], [0.2, 0.23, 0], [0.2, -0.23, 0]])
 
 def FindMax(maps):
@@ -43,10 +45,10 @@ def NonMaximaSuppression(map): # Non-maxima suppression
 
 # cap = cv2.VideoCapture("/mnt/HDD/dataset/module89/V4_vdo+labels/20/62.avi")
 cap = cv2.VideoCapture(0)
-cap.set(3, 640)
-cap.set(4, 480)
-# cap.set(3, 1920)
-# cap.set(4, 1080)
+# cap.set(3, 640)
+# cap.set(4, 480)
+cap.set(3, 1920)
+cap.set(4, 1080)
 cap.set(cv2.CAP_PROP_FPS, 30.0)
 # cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter.fourcc('M', 'J', 'P', 'G'))
 cap.set(cv2.CAP_PROP_AUTOFOCUS, 0)
@@ -60,44 +62,33 @@ ort_sess = ort.InferenceSession(model_path, providers=['TensorrtExecutionProvide
 while True:
     stamp = time.time()
     image = cap.read()[1]
-    if image.shape == (1080, 1920, 3): image = imutils.resize(image, height=480)[:, 106:106 + 640]
-    # if image.shape != (480, 640, 3):
-    #     if image.shape[0] / image.shape[1] > 480/640:   # Use height as reference
-    #         image = imutils.resize(image, height=480)
-    #         delta = 640 - image.shape[1]
-    #         image = cv2.copyMakeBorder(image, 0, 0, int(delta/2), int(delta/2), borderType=cv2.BORDER_CONSTANT, value=(0, 0, 0))
-    #     else:   # Use width as reference
-    #         image = imutils.resize(image, width=640)
-    #         delta = 480 - image.shape[0]
-    #         image = cv2.copyMakeBorder(image, int(delta/2), int(delta/2), 0, 0, borderType=cv2.BORDER_CONSTANT, value=(0, 0, 0))
-
     # convert from NHWC to NCHW (batch N, channels C, height H, width W)
-    x = image.transpose([2, 0, 1])   # ([0, 3, 1, 2])
+    image_480p = imutils.resize(image, height=480)[:, 106:106 + 640]
+    x = image_480p.transpose([2, 0, 1])   # ([0, 3, 1, 2])
     x = np.float32(x)
     x /= 255
     x = np.expand_dims(x, 0)
     outputs = ort_sess.run(None, {'input': x})  # outputs.shape = (1, 4, 60, 80)
     outputs = outputs[0][0]
     overlay = np.zeros(outputs[0].shape, dtype=np.float32)
-    for i in range(4):
-        overlay += outputs[i]
+    for i in range(4): overlay += outputs[i]
     overlay = cv2.cvtColor(overlay, cv2.COLOR_GRAY2BGR)
     overlay = np.array(overlay*255, dtype=np.uint8)
-    overlay = imutils.resize(overlay, height=image.shape[0])
-    canvas = cv2.addWeighted(image, 0.3, overlay, 0.7, 0)
+    overlay = imutils.resize(overlay, height=image_480p.shape[0])
+    canvas = cv2.addWeighted(image_480p, 0.3, overlay, 0.7, 0)
     cv2.imshow("Belief", canvas)
 
-    canvas = image.copy()
+    canvas = image_480p.copy()
     # points, vals = FindMax(outputs[0][0])
     # for i in range(len(points)): cv2.circle(canvas, (points[i][0]*8, points[i][1]*8), 3, (255, 0, 0), -1)
     points, vals = FindMax(outputs)
     # print(vals)
     confidences = [False if val < 0.03 else True for val in vals]
     for i in range(4):
-        cv2.circle(canvas, (points[i][0] * 8, points[i][1] * 8), 3, (255, 0, 0), -1)
+        cv2.circle(canvas, (int(points[i][0] * 8), int(points[i][1] * 8)), 3, (255, 0, 0), -1)
         # if confidences[i] is True:
         #     cv2.circle(canvas, (points[i][0]*8, points[i][1]*8), 3, (255, 0, 0), -1)
-
+    cv2.imshow("Detected points", canvas)
     # obj_points, img_points = [], []
     # for i in range(5):
     #     if confidences[i] is True:
@@ -120,9 +111,14 @@ while True:
     #                        length=0.1)
 
     if not False in confidences:
+        canvas = image.copy()
         obj_points = np.array([[-0.2, 0.23, 0], [-0.2, -0.23, 0], [0.2, 0.23, 0], [0.2, -0.23, 0]])
         img_points = np.array([points[0], points[1], points[2], points[3]], dtype=np.double) * 8
 
+        # map points 480p -> 1080p
+        scale = 1080 / 480
+        img_points = np.array([((point[0] + 106) * scale, point[1] * scale) for point in img_points], dtype=np.double)
+        print(img_points)
         ret, rvec, tvec = cv2.solvePnP(objectPoints=obj_points,
                                        imagePoints=img_points,
                                        cameraMatrix=cameraMatrix,
@@ -134,8 +130,8 @@ while True:
                            rvec=rvec,
                            tvec=tvec,
                            length=0.1)
+        cv2.imshow('Estimated Pose', canvas)
     print("FPS: %d"%(int(1/(time.time() - stamp))))
-    cv2.imshow('A', canvas)
     key = cv2.waitKey(1)
     if key == ord('q'): break
 
